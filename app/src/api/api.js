c3582f97f1c69d29be18a1b11be38f386f6d7f85
eris00 <erissutkovic00@gmail.com>
2025-07-20 20:56:01 +0200

    interceptor hotfix

--- app/src/api/api.js
+++ app/src/api/api.js
@@ -7,10 +7,20 @@ const api = axios.create({
 const DEV_FALLBACK_TOKEN = ""; // token from Postman
 
 api.interceptors.request.use((config) => {
+  config.headers['Accept'] = 'application/json';
+
   let token = localStorage.getItem('token');
   if (!token) token = DEV_FALLBACK_TOKEN;
-  config.headers['Accept'] = 'application/json';
   if (token) config.headers['Authorization'] = `Bearer ${token}`;
+
+  if (
+    config.data &&
+    typeof config.data === 'object' &&
+    !(config.data instanceof FormData)
+  ) {
+    config.headers['Content-Type'] = 'application/json';
+  }
+
   return config;
 });
 
--- app/vite.config.js
+++ app/vite.config.js
@@ -3,15 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // TEMPORARY
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://biblioteka.simonovicp.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-      },
-    },
-  },
 });
