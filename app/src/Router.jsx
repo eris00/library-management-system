55f0093d8839e53950160ac55ca3437693cd3701
Ksenija <ksenijaburic@gmail.com>
2025-07-23 12:09:53 +0200

    Registracija sa izmjenama

--- app/src/Router.jsx
+++ app/src/Router.jsx
@@ -1,17 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-import DashboardLayout from './layouts/DashboardLayout/DashboardLayout'
+import DashboardLayout from './layouts/DashboardLayout/DashboardLayout';
 import Activities from "./pages/Activities/Activities";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Register from "./pages/Register/Register";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path='/' element={<DashboardLayout />}>
-        <Route path="/activities" element={<Activities />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={<DashboardLayout />}>
+        <Route path="activities" element={<Activities />} />
+        <Route path="dashboard" element={<Dashboard />} />
       </Route>
+      <Route path="/register" element={<Register />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
--- app/src/api/UsersServices.js
+++ app/src/api/UsersServices.js
@@ -19,3 +19,16 @@ export const getUser = async (userId) => {
     throw error;
   }
 }
+
+export const registerUser = async (userData) => {
+  try {
+    const response = await api.post('/register', userData);
+    const token = response.data.data.token;
+
+    localStorage.setItem("token", token);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
